9e129b35d326903ada0cdc63930d8b776546a207
AnjumRazaNexusberry <hafizanjumraza1993@gmail.com>
2024-05-16 12:22:12 +0500

    Link one page to another

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl">Hello</h1>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="text-6xl">Next Js</h1>
+      <Link href="/products">Go to products page</Link>
     </main>
   );
 }
